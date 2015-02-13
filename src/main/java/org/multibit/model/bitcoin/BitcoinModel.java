0322acaf472692e393f84c2c4ef1fe4381675d5a
andkryt <andkryt@mail.ru>
2015-02-13 22:32:06 +0200

    Update BitcoinModel.java

--- src/main/java/org/multibit/model/bitcoin/BitcoinModel.java
+++ src/main/java/org/multibit/model/bitcoin/BitcoinModel.java
@@ -87,11 +87,11 @@ public class BitcoinModel extends AbstractModel<CoreModel> {
     public static final String BRING_TO_FRONT = "bringToFront";
 
     // Default fee and feePerKB
-    public static final BigInteger SEND_FEE_DEFAULT = new BigInteger("50000");
-    public static final BigInteger SEND_FEE_PER_KB_DEFAULT = new BigInteger("10000");
+    public static final BigInteger SEND_FEE_DEFAULT = new BigInteger("500");
+    public static final BigInteger SEND_FEE_PER_KB_DEFAULT = new BigInteger("500");
     
     // Minimum fee.
-    public static final BigInteger SEND_MINIMUM_FEE = new BigInteger("10000");
+    public static final BigInteger SEND_MINIMUM_FEE = new BigInteger("500");
 
     // Maximum fee.
     public static final BigInteger SEND_MAXIMUM_FEE = new BigInteger("100000000"); // 1 DGB.
